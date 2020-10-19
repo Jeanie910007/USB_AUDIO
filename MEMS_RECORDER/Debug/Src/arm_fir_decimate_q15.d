871f568da29c51a922ee1647135fd2c0a0ce075f
Jeanie Chiou (BRD-邱群甯) <10007069@MERRY.COM.TW>
2020-10-19 12:06:57 +0800

    add Device

--- MEMS_RECORDER/Debug/Src/arm_fir_decimate_q15.d
+++ MEMS_RECORDER/Debug/Src/arm_fir_decimate_q15.d
@@ -1,16 +1,16 @@
 Src/arm_fir_decimate_q15.o: ../Src/arm_fir_decimate_q15.c \
- C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Inc/arm_math.h \
- C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Drivers/CMSIS/Core/Include/core_cm4.h \
- C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_version.h \
- C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_compiler.h \
- C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_gcc.h
+ C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Inc/arm_math.h \
+ C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Drivers/CMSIS/Core/Include/core_cm4.h \
+ C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_gcc.h
 
-C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Inc/arm_math.h:
+C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Inc/arm_math.h:
 
-C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Drivers/CMSIS/Core/Include/core_cm4.h:
+C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Drivers/CMSIS/Core/Include/core_cm4.h:
 
-C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_version.h:
+C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_version.h:
 
-C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_compiler.h:
+C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_compiler.h:
 
-C:/Users/10007069/STM32CubeIDE/workspace_1.0.2/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_gcc.h:
+C:/Users/10007069/code/USB_AUDIO/MEMS_RECORDER/Drivers/CMSIS/Core/Include/cmsis_gcc.h:
